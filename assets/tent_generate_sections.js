00947cde0b32dad06ce5f40acdfe2e0e79d1af7f
jason.kattenbraker <jason.kattenbraker@fttf.com>
2025-07-22 09:38:59 -0500

    Fixing slight bug with the font style menu

--- assets/tent_generate_sections.js
+++ assets/tent_generate_sections.js
@@ -53,7 +53,7 @@ function generateSections(sectionsConfig) {
                         
                         <div class="custom-dropdown">
                             <button type="button" class="custom-dropdown-button" onclick="toggleDropdown('font-style-${section.id}')">
-                            <span id="font-style-label-${section.id}">Select a font style</span>
+                            Select a font style
                             <svg class="dropdown-arrow" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"/>
                             </svg>
