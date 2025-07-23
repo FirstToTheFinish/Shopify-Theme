6fb02c4d32df409bd93c3164937cc9d37d6a75d8
jason.kattenbraker <jason.kattenbraker@fttf.com>
2025-07-23 14:26:47 -0500

    Adjustment of dropdown menu for art

--- assets/tent_update_art.js
+++ assets/tent_update_art.js
@@ -39,16 +39,16 @@ function addArt(sectionId, sectionTitle) {
                 // Display image in the preview area
                 previewElement = `
                     <div class="art-container resizable" id="${imageId}-preview" style="position: relative; display: inline-block; margin: 5px;" draggable="true" ondragstart="onDragStart(event)" ondragover="onDragOver(event)"  ondragleave="onDragLeave(event)" ondrop="onDrop(event)" ondragend="onDragEnd(event)">
-                        <img src="${e.target.result}" alt="Art Preview" style="max-width: 115px; height: auto;" onload="this.nextElementSibling.nextElementSibling.style.marginTop = this.offsetHeight + 5 + 'px';">
+                        <img src="${e.target.result}" alt="Art Preview" style="max-width: 115px; height: auto;">
                         <button class="remove-art" onclick="removeArt('${imageId}')">&times;</button>
-                        <div class="custom-dropdown">
+                        <div class="custom-dropdown2">
                             <div class="custom-dropdown-button" onclick="toggleDropdown('image-size-${imageId}')">
                                 <span id="selected-image-size-${imageId}">Choose Size</span>
                                 <svg class="dropdown-arrow" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"/>
                                 </svg>
                             </div>
-                            <div class="custom-dropdown-content" id="image-size-${imageId}">
+                            <div class="custom-dropdown-content" id="image-size-${imageId}" style="">
                                 <div onclick="selectImageSizeOption('${imageId}', 'valance-3-4', '3/4 Valance Image')">3/4 Valance Image</div>
                                 <div onclick="selectImageSizeOption('${imageId}', 'valance-full', 'Full Valance Image')">Full Valance Image</div>
                                 <div onclick="selectImageSizeOption('${imageId}', 'peak-partial', 'Partial Peak Image')">Partial Peak Image</div>
