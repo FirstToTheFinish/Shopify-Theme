080904f1f3a68d0bb04d0251b570ae20aa6b1219
jason.kattenbraker <jason.kattenbraker@fttf.com>
2025-07-24 11:35:07 -0500

    up image scale

--- assets/tent-preview.js
+++ assets/tent-preview.js
@@ -101,11 +101,10 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function getSidesConfiguration(hasWalls, size) {
-    const scaleUp = 0.75; // this will increase image size in preview
+    const scaleUp = 0.85; // this will increase image size in preview
     const base = (id, x, y, rotation, title) => ({
         id, x, y, rotation, scale: scaleUp, title
     });
-    console.log("1");
 
     if (hasWalls) {
         return [
