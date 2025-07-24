81ad52d84f15e1355e5cc2a8b92ec0bddf8337a6
jason.kattenbraker <jason.kattenbraker@fttf.com>
2025-07-24 12:00:43 -0500

    pt.26

--- assets/tent-preview.js
+++ assets/tent-preview.js
@@ -115,13 +115,13 @@ function getSidesConfiguration(hasWalls, size) {
             base('BackValanceCanvas',   405, 276,   Math.PI,     'Back'),
             base('BackWallCanvas',      405, 107, Math.PI,     'Back'),
 
-            base('RightPeakCanvas',     718, 378, -Math.PI/2,  'Right'),
+            base('RightPeakCanvas',     713, 378, -Math.PI/2,  'Right'),
             base('RightValanceCanvas',  800, 436, -Math.PI/2, 'Right'),
-            base('RightWallCanvas',     895, 371, -Math.PI/2, 'Right'),
+            base('RightWallCanvas',     894, 371, -Math.PI/2, 'Right'),
 
-            base('LeftPeakCanvas',      300, 378, Math.PI/2,   'Left'),
+            base('LeftPeakCanvas',      325, 378, Math.PI/2,   'Left'),
             base('LeftValanceCanvas',   218, 436, Math.PI/2,   'Left'),
-            base('LeftWallCanvas',      135, 371, Math.PI/2,   'Left')
+            base('LeftWallCanvas',      124, 371, Math.PI/2,   'Left')
         ];
     } else {
         return [
