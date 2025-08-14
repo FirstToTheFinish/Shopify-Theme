efa0f79b83ff4a102408f07a55c4d3763473bb72
jason.kattenbraker <jason.kattenbraker@fttf.com>
2025-08-14 10:49:29 -0500

    Styling changes pt.7

--- assets/track_singlet_uniform_generate_sections.js
+++ assets/track_singlet_uniform_generate_sections.js
@@ -62,13 +62,16 @@ function generateSections(sectionsConfig) {
 
                             <label for="columnNumber1">Column:</label>
                             <input type="number" id="columnNumber1" class="input-field" placeholder="0" min="0" max="34">
-
-                            <!-- Color display box -->
+                        </div>
+                        <div>
                             <div id="colorDisplayBox1" onclick="selectColor(document.getElementById('colorDisplayBox1').style.backgroundColor, 'selectedColor1', 'colorDropdown1')"></div>
                             <span id="colorText1" class="color-text"></span>
                         </div>
                         <!-- Hexcode input field to find the closest color -->
-                        <h2 class="fs16" style="margin-top: 20px;">Trouble Finding Match! Type in color code below and get top 3 closest matches.</h2>
+                        <div class="section-title" style="border-top: 1px solid #e5e7eb;">
+                            <span class="badge">3</span>
+                            <h5>Find Your Top 3 Color Matches</h5>
+                        </div>
                         <div class="input-container">
                             <label for="hexInput1">Hex Code:</label>
                                 <div style="position: relative; display: inline-block;">
