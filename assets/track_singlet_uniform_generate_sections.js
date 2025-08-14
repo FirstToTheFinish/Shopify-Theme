5e568cae0c9ddbfeca3b016e9aae81433e6158d6
jason.kattenbraker <jason.kattenbraker@fttf.com>
2025-08-14 12:15:31 -0500

    pt.14

--- assets/track_singlet_uniform_generate_sections.js
+++ assets/track_singlet_uniform_generate_sections.js
@@ -88,10 +88,10 @@ function generateSections(sectionsConfig) {
                             </div>
                             <!-- "OURS" section for the top 3 closest matches -->
                             <div style="margin-top: 20px;">
-                                <span>Our Colors:</span>
-                                <div id="colorPreview1.1" class="color-preview-box"><div id="closestColorInfo1.1" class="color-text"></div></div><br>
-                                <div id="colorPreview1.2" class="color-preview-box"><div id="closestColorInfo1.2" class="color-text"></div></div><br>
-                                <div id="colorPreview1.3" class="color-preview-box"><div id="closestColorInfo1.3" class="color-text"></div></div><br>
+                                <span style="font-weigth: bold;">Our Colors:</span>
+                                <br><div id="colorPreview1.1" class="color-preview-box"></div><div id="closestColorInfo1.1" class="color-text"></div>
+                                <br><div id="colorPreview1.2" class="color-preview-box"></div><div id="closestColorInfo1.2" class="color-text"></div>
+                                <br><div id="colorPreview1.3" class="color-preview-box"></div><div id="closestColorInfo1.3" class="color-text"></div>
                             </div>
 
                             <!-- Info for the top 3 closest colors -->
