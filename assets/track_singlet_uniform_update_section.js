f28a8fc8710ecf205e5c17b8e690e37636c0efe3
jason.kattenbraker <jason.kattenbraker@fttf.com>
2025-08-18 10:51:23 -0500

    pt.8

--- assets/track_singlet_uniform_update_section.js
+++ assets/track_singlet_uniform_update_section.js
@@ -147,6 +147,14 @@ function selectDropdownOption(dropdownId, value, sectionId) {
         }
     }
 
+    if (dropdownId.includes('font-style') && value != '') {
+    }
+    else{
+        if(value == ''){
+            value = 'Select a Font Effect'
+        }
+    }
+
     
     button.innerHTML = `${value} <svg class="dropdown-arrow" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 24 24" stroke="currentColor">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"/></svg>`;
